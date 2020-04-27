494381666d3d832113fdb1753e2a666e34302078
s22uvansh <s22ubbu@yahoo.com>
2020-04-27 21:01:07 +0530

    solving backend issue v2

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,18 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
-
+var url
+if(process.env.NODE_ENV==="production")
+{
+    url='https://onilne-quiz-portal.herokuapp.com'
+}
+else
+{
+    url='https://localhost:3001'
+}
 module.exports = function(app) {
     app.use(
         '/database',
         createProxyMiddleware({
-            target:'http://localhost:3001',
+            target:url,
             changeOrigin:true
         })
     )
