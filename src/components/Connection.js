763b8954362815ba81b6373b62e2efd5e159cf58
s22uvansh <s22ubbu@yahoo.com>
2020-04-27 20:20:11 +0530

    solving backend issue

--- src/components/Connection.js
+++ src/components/Connection.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
- const api=axios.create({baseURL:"http://localhost:3001/database"})
+ const api=axios.create({baseURL:"http://localhost:3000/database"})
 
  export const getData = ()=>api.get(`/quizdata`)
  export const getDataById = id =>api.get(`/quizdata/${id}`)
